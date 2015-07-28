b8cbce5c8cd68f5a9fd65a31855708b963d6729d
Benedict Aluan <benedict.aluan@gmail.com>
2015-07-28 19:33:09 +1200

    Added placeholder, updated translation file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require turbolinks
 //= require codemirror
 //= require codemirror/modes/ruby
+//= require codemirror.placeholder
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
