c26ad31544a1432193d8f4ff739de195e7cce580
Benedict Aluan <benedict.aluan@gmail.com>
2015-07-29 19:35:32 +1200

    Moved views to a partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = t('app.title')
+
+    if page_title.empty?
+      "#{base_title} | #{t('app.description')}"
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
